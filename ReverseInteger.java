b18d438b130f76c16dbd3afe61caa86c18dd1e16
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-06-09 16:47:59 +0900

    Add files via upload

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,15 +1,15 @@
 
-
 public class ReverseInteger {
 	public static void main(String[] args) {
 		ReverseInteger r = new ReverseInteger();
 		ReverseInteger.Solution s = r.new Solution();
-		int val = s.reverse(123456789);
+		int val = s.reverse(1534236461);
 		System.out.println(val);
 	}
 	
 	class Solution {
 	    public int reverse(int x) {
+	    	
 	        int reverseValue = 0;
 	        int cnt = 0;
 	        int xs = x;
@@ -22,21 +22,20 @@ public class ReverseInteger {
 	        }
 	        	if(val != 1000000000)
 	        		val /=10;
-	        int absX = Math.abs(x);  //Àý´ë°ª
+	        int absX = x;
 	        int d = 1;
 	        for(int i = 1; i <= cnt ; i++) {
-	        	if(absX / val != 0)
-	        	reverseValue+=absX / val * d;
+	        	if(absX / val != 0) {
+	        		reverseValue+=absX / val * d;
+	        	}
 	        	
 	        	absX = absX - (absX / val) * val;
 	        	if(val/ 10  != 0)
-	        	val /= 10;
-	        	d *= 10;
+	        		val /= 10;
+	        	if(d*10  <= 1000000000)
+	        		d *= 10;
 	        }
-	        if(x < 0)
-	        	return -reverseValue;
-	        else
-	        	return reverseValue;
+	        return reverseValue;
 	    }
 	}
 }
