c74fd64be6d2fc7618405b0fccbc21c42710bc89
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-06-19 16:56:24 +0900

    Add files via upload

--- StackCalcultate.java
+++ StackCalcultate.java
@@ -3,48 +3,48 @@ import java.util.*;
 public class StackCalcultate {
 	public static void main(String[]args) {
 		Solution s = new StackCalcultate().new Solution();
-		String s1 = s.calculate("2+3*5");
-		System.out.println(s1.toString());
+		int val = s.calculate("9/3-8+6*2");
+		System.out.println(val);
 	}
 	class Solution { //후위연산자
 		private int count = 0;
-	    public String calculate(String s) {
-	        Stack<Character> tempChar = new Stack<Character>(); // 접시 스택
-	    	char[] calInfo = new char[s.length()];
+	    public int calculate(String s) {
+	        Stack<String> tempChar = new Stack<String>(); // 접시 스택
+	    	String[] calInfo = new String[s.length()];
 	    	//Integer count = 0;
 	       for(int i = 0; i<s.length();i++){
 	        	if(s.charAt(i) >='1' && s.charAt(i) <= '9')
 	        	{
-	        		calInfo[count] = s.charAt(i); 	count++;
+	        		calInfo[count] = Character.toString(s.charAt(i)); 	count++;
 	        	}
 	        	else {
 	        		int n1 = 0; //현재 들어온 숫자의 우선순위값저장 
 	    			int n2 = 0; // 스택우선순위값저장
-	    			Character c = s.charAt(i);
+	    			String c = Character.toString(s.charAt(i));
 	    			if(c!=null)
 	    			switch (c) {
-	    			case '*':
-	    			case '/':
+	    			case "*":
+	    			case "/":
 	    				n1 = 2;
 	    				break;
-	    			case '-':
-	    			case '+':
+	    			case "-":
+	    			case "+":
 	    				n1 = 1;
 	    				break;
 	    			default:
 	    				break;
 	    			}
-	    			Character tmp = null;
+	    			String tmp = null;
 	    			if(!tempChar.empty())
 	    			{
 	    			tmp = tempChar.pop();
 	    			switch (tmp) {
-	    			case '*':
-	    			case '/':
+	    			case "*":
+	    			case "/":
 	    				n2 = 2;
 	    				break;
-	    			case '-':
-	    			case '+':
+	    			case "-":
+	    			case "+":
 	    				n2 = 1;
 	    				break;
 	    			default:
@@ -52,16 +52,17 @@ public class StackCalcultate {
 	    			}
 	    			}
 	    			if(n1 == n2) {
-	    				calInfo[count++] = c;
+	    				calInfo[count++] = tmp;
 	    				tempChar.add(c);
 	    			}else if(n1 == 2 && n2 == 0) {
-	    				calInfo[count++] = c;
+	    				tempChar.add(c);
 	    			}else if(n1 == 2 && n2 == 1) {
 	    				tempChar.add(tmp);
 	    				tempChar.add(c);
 	    			}else if(n1 == 1 && n2 == 0) {
 	    				tempChar.add(c);
 	    			}else if(n1 < n2) {
+	    				calInfo[count++] = tmp;
 	    				while(!tempChar.empty()) {
 	    					calInfo[count++] = tempChar.pop();
 	    				}tempChar.add(c);
@@ -71,11 +72,80 @@ public class StackCalcultate {
 	       while(!tempChar.empty()) {
 				calInfo[count++] = tempChar.pop();
 			}
-	       	return new String(calInfo);
+	       	return getValue(calInfo);
 	    }
-	    public void Record(Character c, Stack<Character> tempStack, char[] info) {
+	    public int getValue(String[] s) {
+			Stack<String> numberStack = new Stack<String>();
+			int val = 0;
+			for(int i = 0; i<s.length;i++) {
+				try {
+				if(Integer.parseInt(s[i]) >= Integer.MIN_VALUE && Integer.parseInt(s[i]) <= Integer.MAX_VALUE)
+	        	{
+	        		numberStack.add(s[i]);
+	        	}
+				}catch(NumberFormatException e) {
+					String s1 = numberStack.pop();
+	        		String s2 = numberStack.pop();
+					int num1 = Integer.parseInt(s1);
+					int num2 = Integer.parseInt(s2);
+	        		switch (s[i]) {
+					case "+": 
+						val = num1 + num2;
+						numberStack.add(Integer.toString(val));
+						break;
+					case "-":
+						val = num2 - num1;
+						numberStack.add(Integer.toString(val));
+						break;
+					case "*":
+						val = num1 * num2;
+						numberStack.add(Integer.toString(val));
+						break;
+					case "/":
+						val = num2 / num1;
+						numberStack.add(Integer.toString(val));
+						break;
+					default:
+						break;
+					}
+				}
+			}
 			
+			val = Integer.parseInt(numberStack.pop());
+			return val;
+	    }
 	}
-	}
-	
 }
+
+
+//else {
+//	String s1 = numberStack.pop();
+//	String s2 = numberStack.pop();
+//	int num1 = Integer.parseInt(s1);
+//	int num2 = Integer.parseInt(s2);
+//	switch (s[i]) {
+//	case "+": 
+//		val = num1 + num2;
+//		numberStack.add(Integer.toString(val));
+//		break;
+//	case "-":
+//		val = num1 - num2;
+//		numberStack.add(Integer.toString(val));
+//		break;
+//	case "*":
+//		val = num1 * num2;
+//		numberStack.add(Integer.toString(val));
+//		break;
+//	case "/":
+//		val = num1 / num2;
+//		numberStack.add(Integer.toString(val));
+//		break;
+//	default:
+//		break;
+//	}
+//}
+
+
+
+
+
