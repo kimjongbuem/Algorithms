e07225f24ea46a0f075496a1e2778ee273ed5fcc
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-06-23 16:49:48 +0900

    Add files via upload

--- player.java
+++ player.java
@@ -18,7 +18,18 @@ public class player {
 	       return participant[i]; 
 	    }
 	}
-	
-	
-	
+}
+class BinaryQ{
+	class Solution {
+	    public TreeNode searchBST(TreeNode root, int val) {
+	        if(root == null)
+	            return null;
+	        if(root.val == val)
+	            return root;
+	        else if(root.val < val)
+	            return searchBST(root.right,val);
+	        else
+	            return searchBST(root.left,val);
+	    }
+	}
 }
