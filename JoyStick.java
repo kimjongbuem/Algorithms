bb01b07208bcf5de25a656e39eb7a75b401ed74d
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-10-15 23:20:42 +0900

    Add files via upload

--- JoyStick.java
+++ JoyStick.java
@@ -27,14 +27,16 @@ class Solution {
 				answer++; ch++;
 			}
     	}else {
-			char ch = 'Z';
-			while(ch != charData[curidx]) 
+			char ch = 'Z'; answer++;
+			while(ch != charData[curidx]) {
 				answer++; ch--;
+			}
 		}
         
     	int leftSize = 0, rightSize = 0;
     	
     	int first = 0; int last = name.length() - 1;
+    	if(first == cur)
     	for(int i = first + 1; i <= last; ++i) {   		
     		if(charData[i] == 'A' && i == last) return answer;
     		else if (charData[i] == 'A') {
@@ -45,8 +47,22 @@ class Solution {
     			break;
     		}
     	}
-    	for(int i = last; i >= first + 1; --i) {   		
-    		if(charData[i] == 'A' && i == first + 1) return answer;
+    	else
+    		for(int i = first; i < last; ++i) {   		
+        		if(charData[i] == 'A' && i == last - 1) return answer;
+        		else if (charData[i] == 'A') {
+        			leftSize++;
+        		}
+        		else {
+        			leftSize++;
+        			break;
+        		}
+        	}
+    		
+    		
+    	if(last==cur)
+    	for(int i = last - 1; i >= first; --i) {   		
+    		if(charData[i] == 'A' && i == first) return answer;
     		else if (charData[i] == 'A') {
     			rightSize++;
     		}
@@ -55,11 +71,22 @@ class Solution {
     			break;
     		}
     	}
-
-    	if(leftSize < rightSize) answer+=RepeatFunc(name.substring(curidx+leftSize)
-    			,first+leftSize);
+    	else
+    		for(int i = last; i >= first + 1; --i) {   		
+        		if(charData[i] == 'A' && i == first + 1) return answer;
+        		else if (charData[i] == 'A') {
+        			rightSize++;
+        		}
+        		else {
+        			rightSize++;
+        			break;
+        		}
+        	}
+    	
+    	if(leftSize <= rightSize) answer+=RepeatFunc(name.substring(curidx+leftSize)
+    			,first+leftSize) + 1;
     	else answer+=RepeatFunc(name.substring(curidx + 1 , name.length() - rightSize + 1),
-    			 last - rightSize);
+    			 last - rightSize) + 1;
     	
     	return answer;
     }
@@ -70,8 +97,6 @@ class Solution {
 public class JoyStick {
 
 	public static void main(String[] args) {
-		System.out.println(new Solution().solution("JAN"));
-
+		System.out.println(new Solution().solution("JEROEN"));
 	}
-
 }
