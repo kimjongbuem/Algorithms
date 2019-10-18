ac4ba3ad8b188e6816250871aea635deaac59a76
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-10-18 16:49:18 +0900

    Add files via upload

--- IronStick.java
+++ IronStick.java
@@ -1,45 +1,61 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 class Solution {
     public int solution(String arrangement) {
-        int answer = 0;
+//        int answer = 0;
+//        
+//        Queue<Character> queue = new LinkedList<Character>();
+//        boolean isStick = false;
+//       int rayer = 0;
+//       char before = 0;
+//       String str = arrangement;
+//       while(true) {
+//
+//       StringBuilder temp= new StringBuilder();
+//       int rf = 0;
+//       int beforeAnswer = answer;
+//        for(int i = 0; i < str.length(); i++) {      	
+//        	if(str.charAt(i) == '(') {
+//        		if(queue.isEmpty()) rf = i;
+//        		queue.add(str.charAt(i));
+//        		before = str.charAt(i);
+//        	}
+//        	else {
+//        		 if(before == '(') rayer++;
+//        		 before =str.charAt(i); queue.poll(); 
+//        		if(!queue.isEmpty()) isStick = true;
+//        		else {
+//        			if(isStick) {
+//        				answer+= (rayer + 1);
+//        				temp.append(str.substring(rf + 1, i ));
+//        			}
+//        			isStick = false; rayer = 0;
+//        		}
+//        	}
+//        }
+//        if(beforeAnswer == answer) break;
+//         str = temp.toString();
+//       }
+//        return answer;
+//    }
+    	int answer = 0;
+        Stack<Character> stack = new Stack<Character>();
         
-        Queue<Character> queue = new LinkedList<Character>();
-        boolean isStick = false;
-       int rayer = 0;
-       char before = 0;
-       String str = arrangement;
-       while(true) {
-
-       String temp="";
-       int rf = 0;
-       int beforeAnswer = answer;
-        for(int i = 0; i < str.length(); i++) {      	
-        	if(str.charAt(i) == '(') {
-        		if(queue.isEmpty()) rf = i;
-        		queue.add(str.charAt(i));
-        		before = str.charAt(i);
-        	}
-        	else {
-        		 if(before == '(') rayer++;
-        		 before =str.charAt(i); queue.poll(); 
-        		if(!queue.isEmpty()) isStick = true;
-        		else {
-        			if(isStick) {
-        				answer+= (rayer + 1);
-        				temp+= str.substring(rf + 1, i );
-        			}
-        			isStick = false; rayer = 0;
-        		}
-        	}
+        for (int i = 0; i < arrangement.length(); i++) {
+            if (arrangement.charAt(i) == '(')
+                stack.add(arrangement.charAt(i));
+            else {
+                stack.pop();
+                if (arrangement.charAt(i-1) == '(')
+                    answer+=stack.size();
+                else
+                    answer++;
+            }
         }
-        if(beforeAnswer == answer) break;
-         str = temp;
-       }
         return answer;
-    }
-}
+}}
 public class IronStick {
 
 	public static void main(String[] args) {
