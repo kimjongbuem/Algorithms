1eebfacf149b1aec0ad2095631fc153615a3a74b
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-10-19 14:14:02 +0900

    Add files via upload

--- FindDecimal.java
+++ FindDecimal.java
@@ -1,56 +1,59 @@
-import java.util.HashSet;
-import java.util.Set;
 
-class Solution {
-    public int solution(String numbers) {
-       Set<Integer> set = new HashSet<Integer>();
-       FindDeciaml(numbers.toCharArray(), set,0);
-    	return set.size();
-    }
-    private void FindDeciaml(char[] numbers, Set<Integer> set, int depth) {
-    	if(depth == numbers.length && checkDeciaml(Integer.parseInt(new String(numbers)))) { 
-    		set.add(Integer.parseInt(new String(numbers)));
-    		return;
-    	}
-    	for(int i = depth; i < numbers.length; i++) {
-    		rightRotate(numbers, i, depth);
-    		FindDeciaml(numbers, set, depth + 1);
-    		leftRotate(numbers, i, depth);
-    	}
-    	
-    }
-    public static void rightRotate(char[] numbers, int e, int s) {
-    	char temp = numbers[e];
-		
-		for(int i=e; i>s; i--) {
-			numbers[i] =numbers[i - 1];
-		}
-		numbers[s] = temp;
-	}
-	public static void leftRotate(char[] numbers, int s, int e) {
-    	char temp =numbers[e];
-		for(int i=e; i<s; i++) {
-			numbers[i] = numbers[i + 1];
-		}
-		numbers[s] = temp;
-	}
-    private boolean checkDeciaml(int num) {
-    	
-    	if(num < 2) return false; // 소수아님
-    	else if(num == 0) return true;
-    	else if(num % 2 == 0) return false;
-    	
-    	for(int i = 2; i < num;i++) {
-    		if(num % i  == 0) return false;
-    	}
-    	return true; // 소수
-    }
-}
+import java.util.*;
+
 public class FindDecimal {
+	//	프로그래머스
+	//	완전탐색 - 소수 찾기
+	//	(https://programmers.co.kr/learn/courses/30/lessons/42839)
+    //  Author: developer.charontea@gmail.com
+
 
 	public static void main(String[] args) {
-		System.out.println(new Solution().solution("17"));
-
+		System.out.println(new FindDecimal().solution("17"));
+		System.out.println(new FindDecimal().solution("011"));
 	}
-
+	static boolean[] checked;
+		public int solution(String numbers) {
+			checked = new boolean[numbers.length()];
+			String numberList[] = new String[numbers.length()];
+			for(int i = 0; i<numbers.length();i++) {
+				numberList[i] = String.valueOf(numbers.charAt(i));
+			}
+			Set<Integer> set = new HashSet<Integer>();
+			FindDeciamls("",numberList,set);
+			
+			
+			return set.size();
+			
+	    }
+	    public void FindDeciamls(String numbers, String[] numberList , Set<Integer> set) {
+	    	if(numbers.length() > numberList.length) return;
+	    	
+	    	if(numbers.length() != 0) {
+	    		int nowNumber = Integer.parseInt(numbers);
+	    		if(isPrime(nowNumber)) set.add(nowNumber);
+	    	}
+	    	for(int i = 0; i < numberList.length; i++) {
+	    		if(!checked[i]) {
+	    		checked[i] = true;
+	    		FindDeciamls(numbers.concat(numberList[i]), numberList, set);
+	    		checked[i] = false;
+	    		}
+	    	}
+	    	
+	    }
+	    static boolean isPrime(int number){
+	        boolean TF = true;
+	        if(number<2) {
+	        	return false;
+	        }
+	        for(int i=2; i<((number/2)+1); i++){
+	            if(number%i==0){
+	                TF = false;
+	                break;
+	            }
+	        }
+	        return TF;
+	    }	    
 }
+
