8dc7cb8bdd59ea7fb125d8bed86d05b45eca342d
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-10-22 14:22:20 +0900

    Add files via upload

---
+++ MatrixMultiple.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for(int i = 0; i < arr1.length; i++) {
+        	for(int j= 0; j < arr2[0].length; j++) {
+            	int sum = 0;
+            	for(int k= 0; k < arr2[0].length; k++) {
+                	sum += arr1[i][k] * arr2[k][j];               	
+                }
+            	answer[i][j] = sum;
+            }
+        }
+        return answer;
+    }
+}
+public class MatrixMultiple {
+
+	public static void main(String[] args) {
+		int[][] arr1 =  {{1, 4}, {3, 2}, {4, 1}};
+		int[][] arr2 =  {{3,3}, {3,3}};
+		System.out.println(new Solution().solution(arr1, arr2));
+
+	}
+
+}
