f5a25284403a8fbe8949fb3bf29023625f203bef
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-10-31 00:03:27 +0900

    Add files via upload

--- Compression.java
+++ Compression.java
@@ -1,31 +1,37 @@
-import java.awt.List;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 class Solution {
   public int[] solution(String msg) {
-	  int number = 27;
+	  int number = 1;
       int[] answer = {};
       java.util.List<Integer> list = new LinkedList<Integer>();
       Map<String, Integer> map = new HashMap<String, Integer>();
+      
+      for(char i = 'A'; i <= 'Z'; i++) {
+    	  map.put(String.valueOf(i), number++);
+      }
+      number--;
       String cur=String.valueOf(msg.charAt(0)); //String next="";
       for(int i = 1; i< msg.length();i++) {
     	  String temp = cur+String.valueOf(msg.charAt(i));
     	  if(!map.containsKey(temp)) {
-    		  map.put(temp, number);
-    		  list.add(number++);
+    		  map.put(temp, ++number);
+    		  list.add(map.get(cur));
     		  cur = String.valueOf(msg.charAt(i));
     	  }
     	  else {
-    		  if(cur.length() > 1) list.add(map.get(cur));
-    		  else list.add(msg.charAt(msg.length() - 1) - 'A' + 1);
+    		  if(cur.length() > 1) list.add(map.get(temp));
+    		  else list.add(cur.charAt(0)- 'A' + 1);
     		  cur = temp;
     	  }
       }
       cur=String.valueOf(msg.charAt(msg.length() - 1));
       if(cur.length() > 1 ) list.add(map.get(cur));
       else list.add(msg.charAt(msg.length() - 1) - 'A' + 1);
+     answer = list.stream().mapToInt(i->i).toArray();
       
       return answer;
   }
@@ -33,7 +39,8 @@ class Solution {
 public class Compression {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] answer= new Solution().solution("KAKAO");
+		for(int i = 0; i<answer.length;i++) System.out.println(answer[i]);
 
 	}
 
