bf35960ce8528e8daa1547625b60677c4d0b672a
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-11-13 20:55:30 +0900

    Add files via upload

--- ImmigrationTest.java
+++ ImmigrationTest.java
@@ -14,10 +14,49 @@ public class ImmigrationTest {
 	        for(int time : times) {
 	        	list.add(new Tester(time)); //큐에 비어있음에도 불구하고 추가를 못할경우 리스트에서 ㅏㄱ제
 	        }
-	        
+	        int p = 0;
+	        while(!checkEnd(list)) {
+	        	if(isEmptyListAnyMatch(list)) {
+	        		if(p < n ) p++;
+	        		Add(list);
+	        		if(p == n) Remove(list);
+	        	}
+	        	for(int i = 0; i < list.size(); i++) list.get(i).DecrementTime();
+	        	
+	        	answer++;
+	        }
 	        
 	        
 	        return answer;
+	    }
+	    public void Remove(List<Tester> list) {
+	    	for(int i = 0; i < list.size(); i++)
+	    		if(list.get(i).isEmpty()) list.remove(i);
+	    }
+	    
+	    public void Add(List<Tester> list) {
+	    	int minIndex = 0; int min = Integer.MAX_VALUE;
+	    	for(int i = 0; i < list.size(); i++) {
+	    		if(list.get(i).GetWaitingTime() < min) {
+	    			minIndex = i; 
+	    			min = list.get(i).GetWaitingTime();
+	    		}
+	    	}
+	    	list.get(minIndex).Add();
+	    }
+	    public boolean isEmptyListAnyMatch(List<Tester> list) {
+	    	return list.stream().anyMatch(
+	    			l -> {
+	    			if(l.GetTesterDestList() ==null) return true;
+	    			return false;
+	    			});
+	    }
+	    public boolean checkEnd(List<Tester> list) {
+	    	return list.stream().allMatch(
+	    			l -> {
+	    			if(l.GetTesterDestList() ==null) return true;
+	    			return false;
+	    			});
 	    }
 	}
 	class Tester{
@@ -27,10 +66,21 @@ public class ImmigrationTest {
 			this.time = time;
 			testerDesk = new LinkedList<Integer>();
 		}
+		public int GetWaitingTime() {
+			if(testerDesk.isEmpty()) return time;
+			return time + testerDesk.get(0);
+		}
+		
 		public void DecrementTime() {
 			if(testerDesk.isEmpty()) return;
 			testerDesk.set(0, testerDesk.get(0));
 			if(testerDesk.get(0) == 0) testerDesk.remove(0);
+		}
+		public List<Integer> GetTesterDestList(){
+			return testerDesk;
+		}
+		public void Add() {
+			testerDesk.add(time);
 		}
 		public int GetTime() {
 			return time;
