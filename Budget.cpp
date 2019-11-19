a9aa9736839d174aa38e5b8ed4a2fae10116d5b8
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-11-19 20:41:03 +0900

    Add files via upload

--- Budget.cpp
+++ Budget.cpp
@@ -6,22 +6,32 @@ using namespace std;
 int solution(vector<int> budgets, int M) {
 	int answer = 0;
 	sort(budgets.begin(), budgets.end());
-	int r = M % budgets.size();
-	int avg = M / budgets.size();
-	int biggerthanavg = 0;
+
 	long sum = 0;
 	for (int i : budgets) sum += i;
 	if (sum < M) {
 		answer = budgets[budgets.size() - 1];
 		return answer;
 	}
-	for (int i = 0; i < budgets.size(); i++) {
-		if (avg >= budgets[i]) r += (avg - budgets[i]);
-		else biggerthanavg++;
+	int mid = 0; int premid = 0;
+	int min = 0, max = M;
+	int len = budgets.size();
+	while (true) {
+		sum = 0;
+		mid = (max + min) / 2;
+		if (mid == premid) break;
+		for (int i = 0; i < len; i++) {
+			if (mid <= budgets[i]) {
+				sum += mid * (len - i);
+				break;
+			}
+			else sum += budgets[i];
+		}
+		if (sum <= M) min = mid;
+		else max = mid;
+		premid = mid;
 	}
-
-	answer = avg + r / biggerthanavg;
-
+	answer = mid;
 	return answer;
 }
 
