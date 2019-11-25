26c68a9e4eac547f2ce806f6132ca397447ababf
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-11-25 18:21:30 +0900

    Add files via upload

--- Jeoul.java
+++ Jeoul.java
@@ -2,12 +2,13 @@ import java.util.Arrays;
 import java.util.Stack;
 
 public class Jeoul {
-	Stack<Integer> st = new Stack<Integer>();
-	//int[] arr= {1,2,3,4};
+	
+	static int[] arr= {3,1,6,2,7,30,1};
 	public static void main(String[] args) {
 		int[] weight = {3,1,6,2,7,30,1};
+		Arrays.sort(arr);
 		System.out.println(new Jeoul().new Solution().solution(weight));
-		//new Jeoul().nCr(4, 2, 0);
+		//new Jeoul().nCr(7, 3, 0);
 	}
 //	void nCr(int n, int r, int index) {
 //		if( r == 0) showStack();
@@ -23,49 +24,43 @@ public class Jeoul {
 //			nCr(n-1,r, index + 1);
 //		}
 //	}
-	void showStack() {
-		for(int i = 0; i < st.size(); i++){
-            System.out.print(st.get(i)+" ");
-        }
-        System.out.println("");
-
-	}
+//	void showStack() {
+//		for(int i = 0; i < st.size(); i++){
+//            System.out.print(st.get(i)+" ");
+//        }
+//        System.out.println("");
+//
+//	}
 	class Solution {
+		Stack<Integer> st = new Stack<Integer>();
 	    public int solution(int[] weight) {
-	        int answer = 1;
-	        Arrays.sort(weight);
-	        while(true) {
-	        	boolean min = true; 
-	        	for(int i = 0; i < weight.length; i++) {
-	        		if(!min) break; 
-	        		if(Combition(weight, weight.length, i, 0, answer)) {
-	        			min = false;
-	        			break;
-	        		}
-	        	}
-	        	if(min) break; 
-	        	answer++;
-	        }
-	        return answer;
+	    	Arrays.sort(weight); if(weight[0]!=1) return 1; int[] sum = new int[weight.length]; int tmpsum = 0; for(int i=0;i<weight.length;i++) 
+	    	{ tmpsum+=weight[i]; sum[i]=tmpsum; if(i<weight.length-1 && sum[i]+1<weight[i+1]) {
+	    		return sum[i]+1; } }
+	    	return sum[sum.length-1]+1;
+
 	    }
 	    boolean Combition(int[] weight, int n ,int r, int index, int number) { // i를 개수로하자. , 조합 nCr...
 	    	boolean b1, b2;
 	    	if( r == 0) {
+	    		if(st.isEmpty()) return false;
 	    		int sum= 0;
-	    		for(int i = 0; i < st.size(); i++) sum+=i;
+	    		for(int i = 0; i < st.size(); i++) sum+=st.pop();
 	    		if(number == sum) return true; else return false;
 	    	}else if(n == r) {
+	    		if(number == 14 && st.size() == 3) {
+	    			System.out.println("");
+	    		}
+	    		if(st.isEmpty()) return false;
 	    		int sum= 0;
-	    		for(int i = 0; i < weight.length; i++) sum+=i;
+	    		for(int i=0;i<n;i++)st.add(weight[index+i]);
+	    		for(int i = 0; i < n; i++) sum+=st.pop();
 	    		if(number == sum) return true; else return false;
 	    	}else {
 	    		st.add(weight[index]);
 	    		b1 = Combition(weight, n - 1, r - 1, index + 1, number);
-	    		
-	    		st.pop();
-	    		b2 = Combition(weight, n - 1, r - 1, index + 1, number);
-	    		
-	    		if(b1 || b2) return true; else return false;
+	    		if(b1) return true;
+	    		return Combition(weight, n - 1, r, index + 1, number);
 	    	}
 	    }
 	}
