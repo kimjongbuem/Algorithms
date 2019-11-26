b9656987e3fd98386b9c9d0e5a56a691adf96d1c
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-11-26 19:59:24 +0900

    Add files via upload

---
+++ Travel.java
@@ -0,0 +1,71 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Travel {
+	public static void main(String[] args) {
+		String[][] tickets ={{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
+		new Travel().new Solution().solution(tickets);
+	}
+	class Solution {
+	    public String[] solution(String[][] tickets) {
+	        String[] answer = {};
+	        LinkedList<Ticket> ticketLink = new LinkedList<Ticket>();
+	        LinkedList<String> answerList = new LinkedList<String>();
+	        for(int i = 0; i < tickets.length; i++) {
+	        	ticketLink.add(new Ticket(tickets[i][0], tickets[i][1]));
+	        }
+	        
+	        for(int i = 0; i < tickets.length; i++) {
+	        	for(int j = 0; j < tickets.length; j++) {
+		        	if(ticketLink.get(i).end.equals(ticketLink.get(j).start)) {
+		        		ticketLink.get(i).link.add(ticketLink.get(j));
+		        		ticketLink.get(j).link.add(ticketLink.get(i));
+		        	}
+		        }
+	        }
+	        Collections.sort(ticketLink);
+	        Ticket start = null;
+	        for(int i = 0; i < ticketLink.size(); i++) {
+	        	if(ticketLink.get(i).start.equals("ICN")) {
+	        		start = ticketLink.get(i); 
+	        		break;
+	        	}
+	        }
+	        
+	        Queue<Ticket> queue = new LinkedList<Ticket>();
+	        queue.add(start);
+	        start.check = true;
+	        while(!queue.isEmpty()) {
+	        	Ticket t = queue.poll();
+	        	if(t.start.equals("ICN")) {
+	        		answerList.add(t.start);
+	        		answerList.add(t.end);
+	        	}else answerList.add(t.end);
+	        	
+	        	for(Ticket tc : t.link) {
+	        		if(!tc.check) {
+	        			tc.check = true;
+	        			queue.add(tc);
+	        		}
+	        	}
+	        }
+	        answer = new String[answerList.size()];
+	        for(int i = 0; i < answerList.size(); i++) answer[i] =answerList.get(i);
+	        return answer;
+	    }
+	    class Ticket implements Comparable<Ticket>{
+	    	String start;
+	    	String end;
+	    	boolean check = false;
+	    	LinkedList<Ticket> link = new LinkedList<Ticket>();
+	    	public Ticket(String start, String end) {
+				this.start = start; this.end = end;
+			}
+			@Override
+			public int compareTo(Ticket o) {
+				return end.compareTo(o.end);
+			}
+	    }
+	}
+}
