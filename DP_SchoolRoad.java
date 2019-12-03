b7197101f95fe4e8cbbd9ce47a2709236c34bda7
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-12-03 21:25:31 +0900

    Add files via upload

--- DP_SchoolRoad.java
+++ DP_SchoolRoad.java
@@ -2,8 +2,8 @@
 public class DP_SchoolRoad {
 
 	public static void main(String[] args) {
-		int[][] puddles = {{2,2}};
-		System.out.println(new DP_SchoolRoad().new Solution().solution(4, 3, puddles));
+		int[][] puddles = {{1,2},{2,2},{3,2},{4,2},{5,4},{2,4},{3,4},{4,4}};
+		System.out.println(new DP_SchoolRoad().new Solution().solution(5, 5, puddles));
 
 	}
 	class Solution {
@@ -17,18 +17,34 @@ public class DP_SchoolRoad {
 	        for(int i = 0; i < puddles.length;i++) {
 	        	map[puddles[i][1] - 1][puddles[i][0] - 1] = true; // -1은 못움직임.
 	        }
-	        answer = recursive(m,n, puddles, 0,0,0);
+	        answer = recursive(m,n, 0,0,0);
 	        return answer;
 	    }
-	    public int recursive(int m, int n, int[][] puddles,int sx, int sy,int answer) {
-	    	if(m - 1 ==sx && n - 1 ==sy) return answer - 1;
-	    	else if(map[sy][sx]) return -1;
-	    	else if(m - 1 == sx) return recursive(m,n,puddles,sx,sy + 1,answer+1);
-	    	else if(n - 1 == sy) return recursive(m,n,puddles,sx+ 1,sy,answer+1);
+	    public int recursive(int m, int n, int sx, int sy,int answer) {
+	    	if(sx < 0 || sy < 0) return Integer.MAX_VALUE;
+	    	
+	    	if(m - 1 ==sx && n - 1 ==sy) return (answer - 1)% 1000000007;
+	    	else if(map[sy][sx]) return Integer.MAX_VALUE;
+	    	else if(m - 1 == sx) {
+	    		map[sy][sx] = true;
+	    		int a = recursive(m,n ,sx - 1,sy,answer+1% 1000000007);
+	    		int b= recursive(m,n, sx,sy + 1,answer+1)% 1000000007;
+	    		return(a > b? b : a)% 1000000007;
+	    	}
+	    	else if(n - 1 == sy) {
+	    		map[sy][sx] = true;
+	    		int a = recursive(m,n,sx+ 1,sy,answer+1) % 1000000007;
+	    		int b= recursive(m,n,sx,sy - 1,answer+1) % 1000000007;
+	    		return (a > b? b : a) % 1000000007;
+	    	}
 	    	else {
-	    		int a = recursive(m,n,puddles,sx + 1,sy,answer+1);
-	    		int b = recursive(m,n,puddles,sx,sy + 1,answer+1);
-	    		return (a > b? a : b ) ;
+	    		map[sy][sx] = true;
+	    		int a = recursive(m,n,sx + 1,sy,answer+1) % 1000000007;
+	    		int b = recursive(m,n,sx,sy + 1,answer+1) % 1000000007;
+	    		int c = recursive(m,n,sx - 1,sy,answer+1) % 1000000007;
+	    		int d = recursive(m,n,sx,sy - 1,answer+1) % 1000000007;
+	    		int v1 =  (a > b? b : a) % 1000000007; int v2 =  (c > d? d : c) % 1000000007;
+	    		return (v1>v2?v2:v1) % 1000000007;
 	    	}
 	    }
 	}
