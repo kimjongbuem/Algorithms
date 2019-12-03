44cfe95fbfbf3039a731211fa4f1d5719d299aaa
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-12-03 20:42:30 +0900

    Add files via upload

--- DP_SchoolRoad.java
+++ DP_SchoolRoad.java
@@ -7,18 +7,28 @@ public class DP_SchoolRoad {
 
 	}
 	class Solution {
+		private int x, y ;
+		boolean[][] map;
 	    public int solution(int m, int n, int[][] puddles) {
-	        int answer = func(m, n, puddles, 0);
+	        int answer = 0;
+	        if(m==1 && n==1) return 1;
+	        x = m; y = n;
+	        map = new boolean[n][m];
+	        for(int i = 0; i < puddles.length;i++) {
+	        	map[puddles[i][1] - 1][puddles[i][0] - 1] = true; // -1은 못움직임.
+	        }
+	        answer = recursive(m,n, puddles, 0,0,0);
 	        return answer;
 	    }
-	    public int func(int m, int n, int[][] puddles, int answer) {
-	    	if(n == puddles.length && m == puddles[0].length) return answer;
-	    	else if(n == puddles.length) return func(m + 1, n, puddles, answer+1);
-	    	else if(m == puddles[0].length) return func(m, n + 1, puddles, answer+1);
+	    public int recursive(int m, int n, int[][] puddles,int sx, int sy,int answer) {
+	    	if(m - 1 ==sx && n - 1 ==sy) return answer - 1;
+	    	else if(map[sy][sx]) return -1;
+	    	else if(m - 1 == sx) return recursive(m,n,puddles,sx,sy + 1,answer+1);
+	    	else if(n - 1 == sy) return recursive(m,n,puddles,sx+ 1,sy,answer+1);
 	    	else {
-	    		int a = func(m, n + 1, puddles, answer+1);
-	    		int b = func(m + 1, n, puddles, answer+1);
-	    		return a>b? a:b;
+	    		int a = recursive(m,n,puddles,sx + 1,sy,answer+1);
+	    		int b = recursive(m,n,puddles,sx,sy + 1,answer+1);
+	    		return (a > b? a : b ) ;
 	    	}
 	    }
 	}
