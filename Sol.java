4f01654d323cbebb84e1f4c81085464eed233534
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-12-04 23:24:26 +0900

    Add files via upload

--- Sol.java
+++ Sol.java
@@ -7,7 +7,20 @@ public class Sol {
 	}
 	class Solution {
 		  public int[] solution(int n, int s) {
+			  if(n > s) return new int[] {-1};
 		      int[] answer = {};
+		      answer = new int[n];
+		      int start = s / n;
+		      int remain = s % n;
+		      int i = 0;
+		      for(; i < n; i++)answer[i] = start;
+		      i = 1;
+		      while(remain > 0) {
+		    	  answer[i++] += 1;
+		    	  remain--;
+		    	  if(i == n) i = 1;
+		      }
+		      
 		      return answer;
 		  }
 		}
