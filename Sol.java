a9b2a267517e4a9f7da79677f690c2a4e8a7343a
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-12-04 23:09:48 +0900

    Add files via upload

---
+++ Sol.java
@@ -0,0 +1,14 @@
+
+public class Sol {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	class Solution {
+		  public int[] solution(int n, int s) {
+		      int[] answer = {};
+		      return answer;
+		  }
+		}
+}
