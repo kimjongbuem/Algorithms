27dbe35c952fe6b4f58179994ac6032766a9a042
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-12-07 14:31:47 +0900

    Add files via upload

--- RoadFindingGame.java
+++ RoadFindingGame.java
@@ -10,7 +10,6 @@ public class RoadFindingGame {
 
 	}
 	class Solution {
-		//Node topNode;
 	    public int[][] solution(int[][] nodeinfo) {
 	        int[][] answer = {};
 	        LinkedList<Node> nodelist = new LinkedList<Node>();
@@ -24,97 +23,95 @@ public class RoadFindingGame {
 	        
 	        return answer;
 	    }
-	   void prefix(LinkedList<Node> nodelist,LinkedList<Integer> prefixList, int idx) {
+	    void prefix(LinkedList<Node> nodelist,LinkedList<Integer> prefixList, int idx) {
 	    	Node parent = nodelist.get(idx);
 	    	prefixList.add(nodelist.get(idx).value); // 전위
 	    	
-	    	Node leftNode = null; int leftIdx = -1;
-	    	for(int i = idx + 1; i < nodelist.size(); i++) {
-	    		if(parent.TopNode == null) {
-	    			if(leftNode == null) {
-    					leftNode = nodelist.get(i);
-	    				leftIdx = i;
-    				}else {
-    					if(leftNode.xPos > nodelist.get(i).xPos && leftNode.yPos <= nodelist.get(i).yPos) {
-							leftNode = nodelist.get(i); leftIdx = i;
-						}
-    				}
-	    			
-	    		}
-	    		else if(parent.xPos > nodelist.get(i).xPos && parent.yPos > nodelist.get(i).yPos&&
-						parent.TopNode.xPos > nodelist.get(i).xPos) {
-	    			if(leftNode == null) {
-    					leftNode = nodelist.get(i);
-	    				leftIdx = i;
-    				}	
-    				else {
-    						if(leftNode.xPos < nodelist.get(i).xPos && leftNode.yPos <= nodelist.get(i).yPos) {
-    							leftNode = nodelist.get(i); leftIdx = i;
-    						}
-    					}
-    				}
-	    		}
-	    	
-	    	
-	    	
-	    	Node rightNode = null; int rightIdx = -1;
-	    	for(int i = idx + 1; i < nodelist.size(); i++) { 
-	    		if(parent.TopNode == null) {
-	    			if(rightNode == null) {
-    					rightNode = nodelist.get(i);
-	    				rightIdx = i;
-    				}
-    				else {
-    						if(rightNode.xPos < nodelist.get(i).xPos && rightNode.yPos <= nodelist.get(i).yPos) {
-    							rightNode = nodelist.get(i);
-    							rightIdx = i;
-    						}
-    				}
-	    		}
-
-	    		else if(parent.xPos < nodelist.get(i).xPos && parent.yPos > nodelist.get(i).yPos&&
-						parent.TopNode.xPos > nodelist.get(i).xPos ) {
+	    	Node leftNode = null; Node rightNode = null; int left = -1; int right = -1;
+	    	if(parent.TopNode == null) {
+	    		for(int i = idx + 1; i < nodelist.size();i++) {
+	    			if(parent.xPos > nodelist.get(i).xPos &&  parent.yPos > nodelist.get(i).yPos) {
+	    				if(leftNode == null) {
+	    					leftNode = nodelist.get(i);
+	    					left = i;
+	    				}
+	    				else continue;
+	    			}
+	    			else if(parent.xPos < nodelist.get(i).xPos &&  parent.yPos > nodelist.get(i).yPos) {
 	    				if(rightNode == null) {
 	    					rightNode = nodelist.get(i);
-		    				rightIdx = i;
+	    					right = i;
 	    				}
-	    				else {
-	    						if(rightNode.xPos > nodelist.get(i).xPos && rightNode.yPos <= nodelist.get(i).yPos) {
-	    							rightNode = nodelist.get(i); rightIdx = i;
-	    						}
+	    				else continue;
+	    			}
+	    		}
+	    	}else {
+	    		Node up = parent.TopNode;
+	    		if(up.xPos > parent.xPos) {
+	    			for(int i = idx + 1; i < nodelist.size(); i++) {
+	    				if(parent.xPos > nodelist.get(i).xPos && parent.yPos > nodelist.get(i).yPos) {
+	    					if(leftNode == null) {
+	    						leftNode = nodelist.get(i);
+	    						left = i;
+	    					}
+	    					else continue; // ? 수정예상곳
 	    				}
+	    				else if(parent.xPos < nodelist.get(i).xPos && parent.yPos > nodelist.get(i).yPos) {
+	    					if(rightNode == null) {
+	    						rightNode = nodelist.get(i);
+	    						right = i;
+	    					}
+	    				}
+	    			}
+	    		}
+	    		else if(up.xPos < parent.xPos) {
+	    			for(int i = idx + 1; i < nodelist.size(); i++) {
+	    				if(parent.xPos > nodelist.get(i).xPos && parent.yPos > nodelist.get(i).yPos) { // left
+	    					if(leftNode == null) {
+	    						leftNode = nodelist.get(i);
+	    						left = i;
+	    					}
+	    				}
+	    				else if(parent.xPos < nodelist.get(i).xPos && parent.yPos > nodelist.get(i).yPos) {
+	    					if(rightNode == null) {
+	    						rightNode = nodelist.get(i);
+	    						right = i;
+	    					}
+	    				}
+	    			}
 	    		}
 	    	}
-	    	if(leftIdx == -1 && rightIdx == -1) return;
-	    	else if(leftIdx == -1) {
+	    	if(left == -1 && right == -1) return;
+	    	else if(left== -1) {
 	    		rightNode.TopNode = parent;
-	    		prefix(nodelist, prefixList, rightIdx);
+	    		prefix(nodelist, prefixList, right);
 	    	
 	    	}
-	    	else if(rightIdx == -1) {
+	    	else if(right== -1) {
 	    		leftNode.TopNode = parent;
-	    		prefix(nodelist, prefixList, leftIdx);
+	    		prefix(nodelist, prefixList, left);
 	    	}
 	    	else {
 	    		rightNode.TopNode = parent;
 	    		leftNode.TopNode = parent;
-	    		prefix(nodelist, prefixList, leftIdx); // 왼쪽 ㄱㄱ
-		    	prefix(nodelist, prefixList, rightIdx);// 오른쪽 ㄱㄱ
+	    		prefix(nodelist, prefixList, left); // 왼쪽 ㄱㄱ
+		    	prefix(nodelist, prefixList, right);// 오른쪽 ㄱㄱ
 	    	}
-	    
 	    }
+
+	    
 		class Node implements Comparable<Node>{
 			int xPos; int yPos;
 			int value; Node TopNode;
-			boolean checked = false;
 			
 			Node(int x, int y ,int v){
 				xPos =x; yPos = y; value =v;
 			}
-			
+
 			@Override
 			public int compareTo(Node o) {
-				return o.yPos - yPos;
+			if(yPos == o.yPos)return xPos - o.xPos;
+			else return o.yPos - yPos;
 			}
 		}   
 	}
