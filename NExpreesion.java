4d79e61ecebafa59d3edba3dc89ca07fe0d5caa6
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-12-11 22:39:47 +0900

    Add files via upload

--- NExpreesion.java
+++ NExpreesion.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 public class NExpreesion {
 
 	public static void main(String[] args) {
-		System.out.println(new NExpreesion().new Solution().solution(12, 5));
+		System.out.println(new NExpreesion().new Solution().solution(5, 12));
 
 	}
 	class Solution {
@@ -12,55 +12,80 @@ public class NExpreesion {
 		LinkedList<Integer> list = new LinkedList<Integer>();
 		
 	    public int solution(int N, int number) {
-	        //dfs(N,number,0,0);
-	    	return min< 9 ? min : -1;
+	       // dfs(N,number,0,0);
+	    	BFS(N, number);
+	    	return min < 9 ? min : -1;
 	    }
 	    public void BFS(int N, int number) {
 	    	if(count >= min) return;
 	    	if(list.size() >= min) return;
 	    	int lastNumber = (list.size() == 0) ? 0 : list.get(list.size() - 1);
-	    	if(lastNumber == N) {
+	    	if(lastNumber == number) {
 	    		min = min > count ? count : min; return;
 	    	}
 	    	int n = 0; int addCount = 0;
 	    	for(int c = 1; c<=1000000; c*=10) {
+	    		addCount++;
+	    		if(count +addCount >= min) break;
+	    		n += (c * N);
+	    		count += addCount;
 	    		
+	    		list.add(lastNumber + n);
+	    		BFS(N, number);
+	    		list.pop();
+	    		
+	    		if(lastNumber - n != 0) {
+	    			list.add(lastNumber - n);
+		    		BFS(N, number);
+		    		list.pop();
+	    		}
+	    		
+	    		list.add(lastNumber * n);
+	    		BFS(N, number);
+	    		list.pop();
+	    		
+	    		if(lastNumber / n != 0) {
+	    			list.add(lastNumber / n);
+		    		BFS(N, number);
+		    		list.pop();
+	    		}
+	    		count -= addCount;
 	    	}
 	    }
 	    
-//	    public void dfs(int N, int number, int start, int value){
-//	    	for(int i = 0; i<=100000;i*=10) {
-//	    		int val = (i*number) + number; // 사칙연산.. 
-//	    		int temp = i + 1; if(min < temp) return;
-//	    		// basic
-//	    		if(val + value == N) {
-//	    			if(min > temp) min = temp;
-//	    			return;
-//	    		}else {
-//	    			int tempVal;
-//	    			// +
-//	    			tempVal = val + value;
-//	    			if(tempVal == N)if(min > temp+ 1) {
-//	    				min = temp+1;
-//	    			} else dfs(N, number ,++start,tempVal);
-//	    			// -
-//	    			tempVal = value - val;
-//	    			if(tempVal == N)if(min > temp + 1) {
-//	    				min = temp + 1;
-//	    			}else if(tempVal <= 0) {}
-//	    			else dfs(N, number ,++start,tempVal);
-//	    			// *
-//	    			tempVal = val * number;
-//	    			if(tempVal == N)if(min > temp + 1) {
-//	    				min = temp + 1;
-//	    			}else dfs(N, number ,++start,tempVal);
-//	    			// /
-//	    			tempVal = number/val;
-//	    			if(tempVal == N)if(min > temp) {
-//	    				min = temp;
-//	    			}else dfs(N, number ,++start,tempVal);
-//	    		}
-//	    	}
-//	    }
+	    public void dfs(int N, int number, int start, int value){
+	    	for(int i = 0; i<=100000;i*=10) {
+	    		int val = (i*number) + number; // 사칙연산.. 
+	    		int temp = i + 1; if(min < temp) return;
+	    		// basic
+	    		if(val + value == N) {
+	    			if(min > temp) min = temp;
+	    			return;
+	    		}else {
+	    			int tempVal;
+	    			// +
+	    			tempVal = val + value;
+	    			if(tempVal == N)if(min > temp+ 1) {
+	    				min = temp+1;
+	    			} else dfs(N, number ,++start,tempVal);
+	    			// -
+	    			tempVal = value - val;
+	    			if(tempVal == N)if(min > temp + 1) {
+	    				min = temp + 1;
+	    			}else if(tempVal <= 0) {}
+	    			else dfs(N, number ,++start,tempVal);
+	    			// *
+	    			tempVal = val * number;
+	    			if(tempVal == N)if(min > temp + 1) {
+	    				min = temp + 1;
+	    			}else dfs(N, number ,++start,tempVal);
+	    			// /
+	    			tempVal = number/val;
+	    			if(tempVal == N)if(min > temp) {
+	    				min = temp;
+	    			}else dfs(N, number ,++start,tempVal);
+	    		}
+	    	}
+	    }
 	}
 }
