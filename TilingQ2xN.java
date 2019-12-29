82e3c75e55fa4f347f9f7080019933d66d035d4b
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-12-29 11:13:32 +0900

    Add files via upload

---
+++ TilingQ2xN.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TilingQ2xN {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int dp[] = new int[1001];
+		dp[1] = 1;
+		dp[2] = 2;
+		int n = sc.nextInt();
+		for(int i = 3; i<=n;i++) {
+			dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+		}
+		System.out.println(dp[n] % 10007);
+	}
+}
