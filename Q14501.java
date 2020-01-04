66b8b81b1075e3f90a2e1b53774f43794709de1c
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2020-01-04 13:35:41 +0900

    Add files via upload

--- Q14501.java
+++ Q14501.java
@@ -1,33 +1,56 @@
 import java.util.Scanner;
 
 public class Q14501 {
-	static int max = 0;
+	static int ans = 0;
+	static int N;
+	static int[] t,p;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[]t = new int[N + 1];
-		int[]p = new int[N + 1];
+		N = sc.nextInt();
+		t = new int[N + 1];
+		p = new int[N + 1];
 		for(int i = 1; i <= N;i++)
 		{
 			t[i] = sc.nextInt();
 			p[i] = sc.nextInt();
 		}
-		for(int i = 1; i <= N;i++) { // 첫시작.
-			int sum = 0;
-			int idx = i;
-			int curT = 1;
-			int nextT = t[i];
-			for(int j = i + 1; j <= N;j++) {
-				if(nextT == curT) {
-					sum += p[idx];
-					idx = j;
-					nextT = t[j];
-					curT = 0;
-				}
-				curT++;
-			}
-			max = max > sum ? max : sum;
+		dfs(1, 0);
+		System.out.println(ans);
+		
+		
+	}
+	public static void dfs(int day, int sum) {
+		if(day == N + 1) {
+			ans = ans > sum ? ans : sum;
+			return;
 		}
-		System.out.println(max);
+		// 상담을 안 하는경우 //
+		dfs(day + 1, sum);
+		if(day + t[day] <= N + 1) dfs(day + t[day], sum + p[day]);
 	}
 }
+
+/*
+//for(int i = 1; i <= N;i++) { // 첫시작.
+//	int sum = 0;
+//	int idx = i;
+//	int curT = 0;
+//	int nextT = t[i]; boolean flag = false;
+//	for(int j = i; j <= N;j++) {
+//		curT++;
+//		if(flag) {
+//			idx = j;
+//			nextT = t[j];
+//			flag = false;
+//		}
+//		if(nextT == curT) {
+//			sum += p[idx];
+//			curT = 0;
+//			flag = true;
+//		}
+//	}
+//	max = max > sum ? max : sum;
+//}
+//System.out.println(max);
+ * */
+ 
