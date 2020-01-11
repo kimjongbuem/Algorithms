38ad8b5e1eb80c7663b92bc5752145d1ed76cce0
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2020-01-11 11:17:31 +0900

    Add files via upload

---
+++ Q9251.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q9251 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		String s2 = sc.next();
+		sc.close();
+		int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+		for(int i = 1; i <= s1.length();i++) {
+			char c1 = s1.charAt(i - 1);
+			for(int j = 1; j <= s2.length();j++) {
+				char c2 = s2.charAt(j - 1);
+				if(c1 == c2) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				}else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				}
+			}
+		}
+		System.out.println(dp[s1.length()][s2.length()]);
+	}
+}
