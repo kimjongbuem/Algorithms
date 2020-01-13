ce835b6e6f2a8530f6d6f976e668067a795db088
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2020-01-13 18:59:45 +0900

    Add files via upload

--- Q12865.java
+++ Q12865.java
@@ -9,31 +9,48 @@ public class Q12865 {
 		int n = sc.nextInt();
 		int K = sc.nextInt();
 		int[][] dp = new int[n + 1][K + 1]; 
-		LinkedList<Stuff> stuffs = new LinkedList<Stuff>();
-		for(int i = 0; i < n; i++) stuffs.add(new Stuff(sc.nextInt(), sc.nextInt()));
-		stuffs.sort(new Comparator<Stuff>() {
-			@Override
-			public int compare(Stuff o1, Stuff o2) {
-				return o2.w - o1.w;
-			}
-		});
-		sc.close();
+		int w[] = new int[n + 1]; int v[] = new int[n + 1];
+		
 		for(int i = 1; i <= n; i++) {
-			Stuff s = stuffs.get(i - 1); // 넣을 아이;
-			for(int k = 0; k <= K; k++) {
-				if(k == 0) dp[i][k] = 0;
-				else if(s.w == k) dp[i][k] = s.v;
-				else if(dp[i - 1][k] != 0 && s.w + k <= K) dp[i][k] = dp[i - 1][k] + s.v;
-				else dp[i][k] = dp[i - 1][k] > dp[i][k - 1]? dp[i - 1][k] : dp[i][k - 1];
+			w[i] = sc.nextInt(); v[i] = sc.nextInt();
+		} sc.close();
+		
+		for(int i = 1; i <= n; i++) {
+			for(int k = 1; k <= K; k++) {
+				dp[i][k] = dp[i - 1][k];
+				if(k - w[i] >= 0) {
+					dp[i][k] = dp[i][k]> dp[i - 1][k - w[i]] + v[i] ? dp[i][k] : dp[i - 1][k - w[i]] + v[i];
+				}
 			}
 		}
 		System.out.println(dp[n][K]);
+		
+//		LinkedList<Stuff> stuffs = new LinkedList<Stuff>();
+//		for(int i = 0; i < n; i++) stuffs.add(new Stuff(sc.nextInt(), sc.nextInt()));
+//		stuffs.sort(new Comparator<Stuff>() {
+//			@Override
+//			public int compare(Stuff o1, Stuff o2) {
+//				return o2.w - o1.w;
+//			}
+//		});
+//		sc.close();
+//		for(int i = 1; i <= n; i++) {
+//			Stuff s = stuffs.get(i - 1); // 넣을 아이;
+//			for(int k = 0; k <= K; k++) {
+//				if(k == 0) dp[i][k] = 0;
+//				else if(s.w == k) dp[i][k] = s.v;
+//				else if(dp[i - 1][k] != 0 && s.w + k <= K) dp[i][k] = dp[i - 1][k] + s.v;
+//				else dp[i][k] = dp[i - 1][k] > dp[i][k - 1]? dp[i - 1][k] : dp[i][k - 1];
+//			}
+//		}
+//		System.out.println(dp[n][K]);
 	}
-	static class Stuff {
-		int v;
-		int w;
-		public Stuff(int w, int v){
-			this.v = v; this.w = w;
-		}
-	}
-}
+//	static class Stuff {
+//		int v;
+//		int w;
+//		public Stuff(int w, int v){
+//			this.v = v; this.w = w;
+//		}
+//	}
+}	
+
