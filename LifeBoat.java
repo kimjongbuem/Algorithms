df106215636ee09ccc0f52d7aea0a937157b2923
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2020-01-15 18:32:59 +0900

    Add files via upload

---
+++ LifeBoat.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class LifeBoat {
+
+	public static void main(String[] args) {
+		int people[] = {70,50,80,50}; int limit = 100;
+		System.out.println(new LifeBoat().new Solution().solution(people, limit));
+	}
+	class Solution {
+	    public int solution(int[] people, int limit) {
+	        int answer = 0;
+	        Integer[] peoples = new Integer[people.length];
+	        int[] containers = new int[people.length];
+	        for(int i = 0; i < people.length; i++) peoples[i] = people[i]; 
+	        Arrays.sort(peoples,Collections.reverseOrder());
+	        for(int i = 0; i < peoples.length; i++) {
+	        	int w = peoples[i]; boolean flag = false;
+	        	for(int j = 0; j <= answer;j++) {
+	        		if(containers[j] + w <= limit) {
+	        			flag = true;
+	        			containers[j] += w;
+	        			break;
+	        		}
+	        	}
+	        	if(!flag) containers[++answer] = w;
+	        }
+	        return answer;
+	    }
+	}
+}
