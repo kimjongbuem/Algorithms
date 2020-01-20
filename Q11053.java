b2b62a1df3bdbea663a56a7c5f9f64b7bfcb6872
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2020-01-20 23:36:52 +0900

    Add files via upload

--- Q11053.java
+++ Q11053.java
@@ -5,17 +5,17 @@ public class Q11053 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int min = 0; int i;
+		 int i; int ans = 0;
 		for(i = 1; i <= n; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = sc.nextInt(); int min = 0;
 			for(int j = 0; j < i; j++) {
 				if(arr[i] > arr[j]) {
 					if(min < dp[j]) min =dp[j];
 				}
 			}
 			dp[i] = min + 1;
+			if(ans < dp[i]) ans = dp[i];
 		}
-		System.out.println(dp[i - 1]);
+		System.out.println(ans);
 	}
-
 }
