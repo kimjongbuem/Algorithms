06bd78e6da47848330569314eae347bd2c0fc11e
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2020-01-20 23:27:35 +0900

    Add files via upload

--- Q11053.java
+++ Q11053.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class Q11053 {
 	static int arr[] = new int[1001]; static int[] dp = new int[1001]; 
 	public static void main(String[] args) {
-		int answer = 0;
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int here = Integer.MIN_VALUE; dp[0] = here;
-		for(int i = 1; i <= n; i++) {
-			int d  = sc.nextInt();
-			if(dp[i - 1] < d) {
-				dp[i] = d; answer++;
-			}else dp[i] = dp[i - 1];
+		int min = 0; int i;
+		for(i = 1; i <= n; i++) {
+			arr[i] = sc.nextInt();
+			for(int j = 0; j < i; j++) {
+				if(arr[i] > arr[j]) {
+					if(min < dp[j]) min =dp[j];
+				}
+			}
+			dp[i] = min + 1;
 		}
-		System.out.println(answer);
+		System.out.println(dp[i - 1]);
 	}
 
 }
