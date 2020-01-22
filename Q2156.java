310d8b7bc3854079a3849f7535dd67a9bf4de5e8
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2020-01-22 19:17:54 +0900

    Add files via upload

--- Q2156.java
+++ Q2156.java
@@ -8,25 +8,32 @@ public class Q2156 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		grasps = new int[n + 1];
+		int dp[] = new int[10001];
 		for(int i = 1; i <= n; i++) grasps[i] = sc.nextInt();
-		re(0);
-		System.out.println(max);
+		for(int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 2] + grasps[i] > dp[i - 3] + grasps[i] + grasps[i - 1] ? dp[i - 2] + grasps[i] : dp[i - 3] + grasps[i] + grasps[i - 1] ;
+			dp[i] = dp[i] > dp[i - 1] ? dp[i] : dp[i - 1];
+		}
+		
+		
+		
+		System.out.println(dp[n]);
  	}
-	public static void re(int curIndex) {
-		if(curIndex >= grasps.length) {
-			int v = list.stream().reduce(0, Integer::sum);
-			max = max > v? max : v;
-		}
-		else {
-			// ø¨º” 1¿‹ , 2¿‹ ∏‘æ˙¿ª∂ß....
-			list.add(grasps[curIndex]);
-			re(curIndex + 2);
-			list.pop();
-			
-			list.add(grasps[curIndex]); 
-			if(curIndex + 1 <= grasps.length - 1) list.add(grasps[curIndex + 1]); 
-			re(curIndex + 2);
-			list.pop(); if(curIndex + 1 <= grasps.length - 1) list.pop();
-		}
-	}
+//	public static void re(int curIndex) {
+//		if(curIndex >= grasps.length) {
+//			int v = list.stream().reduce(0, Integer::sum);
+//			max = max > v? max : v;
+//		}
+//		else {
+//			// ø¨º” 1¿‹ , 2¿‹ ∏‘æ˙¿ª∂ß....
+//			list.add(grasps[curIndex]);
+//			re(curIndex + 2);
+//			list.pop();
+//			
+//			list.add(grasps[curIndex]); 
+//			if(curIndex + 1 <= grasps.length - 1) list.add(grasps[curIndex + 1]); 
+//			re(curIndex + 2);
+//			list.pop(); if(curIndex + 1 <= grasps.length - 1) list.pop();
+//		}
+//	}
 }
