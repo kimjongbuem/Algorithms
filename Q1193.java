963fb824ad5895971d7ac07440793c61223f6cb7
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2020-02-02 00:07:23 +0900

    Add files via upload

--- Q1193.java
+++ Q1193.java
@@ -7,19 +7,28 @@ public class Q1193 {
 		int n = sc.nextInt(); int s = n;
 		int cnt = 0; int val = 1;
 		while(n > 0) {
-			int beforeVal = val;
-			n -= val; val += beforeVal;
+			n -= val; val += 1;
 			cnt++;
+		}
+		val = cnt - 1; n = 0;
+		for(int i = val; i >= 1; i--) {
+			n += i;
 		}
 		// v1 / v2
 		if(cnt % 2 == 0) { //짝수 인 경우
 			int v1 = 1; 
-			int v2		
-			for(int i = 0;i < cnt;i++) {
-				
+			int v2 = cnt;		
+			for(int i = 1;i < s - n;i++) {
+				v1 += 1; v2 -=1;
 			}
+			System.out.println(v1+"/"+v2);
 		}else {
-			
+			int v1 = cnt; 
+			int v2 = 1;		
+			for(int i = 1;i < s - n;i++) {
+				v1 -= 1; v2 +=1;
+			}
+			System.out.println(v1+"/"+v2);
 		}
 	}
 
