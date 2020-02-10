77623891de4c607d894860e9fa13b238d7706bcd
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2020-02-10 19:33:40 +0900

    Add files via upload

---
+++ Q2798.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q2798 {
+	static int N[] = new int[101];
+	static int M;
+	static int RESULT = 0;
+	static int count = 0;
+	static int n;
+	static int answer = 0;
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		n = sc.nextInt();
+		M = sc.nextInt();
+		for(int i = 1;i<=n;i++) N[i] = sc.nextInt();
+		Result(1);
+		System.out.println(answer);
+	}
+	public static void Result(int idx) {
+		if(count >= 3 || RESULT > M) return;
+		else if(count >= 3) answer = answer > RESULT ? answer : RESULT; 
+		else {
+			RESULT += N[idx]; count++;
+			for(int i = idx + 1; i <= n;i++) Result(i);
+			RESULT -= N[idx]; count--;
+		}
+	}
+}
