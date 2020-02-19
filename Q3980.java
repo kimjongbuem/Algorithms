82060baca2fdba9f2a8bccd57472f3e3f2b417f8
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2020-02-19 22:03:15 +0900

    Add files via upload

--- Q3980.java
+++ Q3980.java
@@ -1,15 +1,20 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Q3980 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n= sc.nextInt();
-		int val = 1;
+		BigInteger val = new BigInteger("1");
 		for(int i = 0;i<n;i++) {
-			val*=2;
+			val = val.multiply(new BigInteger("2"));
 		}
 		if(n == 1)System.out.println(1);
-		else System.out.println(val - 1);
+		else { 
+			val = val.subtract(new BigInteger("1"));
+			System.out.println(val);
+		
+		}
 		if(n<=20) HanoiTower(n, 1, 2, 3);
 	}
 	public static void HanoiTower(int n,int from,int center,int to) {
